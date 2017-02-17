2027628444816b93136df4ccbb053ddc64ec09bd
CameronH1993 <camerondavidhughes@gmail.com>
2017-02-17 03:58:09 +0000

    Create user_control.php

---
+++ user_control.php
@@ -0,0 +1,54 @@
+<?php
+
+include_once 'connection.php';
+	
+	class Spatialdata {
+		
+		private $db;
+		private $connection;
+		
+		function __construct() {
+			$this -> db = new DB_Connection();
+			$this -> connection = $this->db->getConnection();
+		}
+		
+		
+		public function sendtodb($gyroscope,$accelerometer)
+		{
+			$query = "UPDATE mysensordata SET gyroscope = $gyroscope accelerometer = $accelerometer WHERE idmysensor = 0";
+			
+			
+		}
+		
+	
+	$spatialdata = new Spatialdata();
+	if(isset($_POST['gyroscope'],$_POST['accelerometer'])) {
+		$gyroscope = $_POST['gyroscope'];
+		$accelerometer = $_POST['accelerometer'];
+		
+		if(!empty($gyroscope) && !empty($accelerometer)){
+			
+		
+		$spatialdata -> sendtodb($gyroscope,$accelerometer);
+			
+			
+		}else{
+			echo json_encode("could not send to DB");
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
